57a5d93cbc7d9192a3b98799849d7426c54e7c9f
Matt Choinski <matt@choinski.net>
2022-12-11 19:43:00 -0500

    Initial implementation of calculating an election winner, but does not take into consideration ties.

--- Elections/Elections/PluralityElection.cs
+++ Elections/Elections/PluralityElection.cs
@@ -1,11 +1,32 @@
-﻿using Elections.Interfaces;
+﻿using System.Collections;
+using Elections.Interfaces;
 
 namespace Elections.Elections;
 
 public class PluralityElection : IElection<ISingleVoteBallot>
 {
-    public ICandidate Run(IReadOnlyList<ISingleVoteBallot> ballots, IReadOnlyList<ICandidate> candidates)
+    public ICandidate Run(
+        IReadOnlyList<ISingleVoteBallot> ballots,
+        IReadOnlyList<ICandidate> candidates )
     {
-        throw new NotImplementedException();
+        if ( ( ballots != null && ballots.Count > 0 )
+            && ( candidates != null && candidates.Count > 0 ) )
+        {
+            // Note: Initial 'brute force' approximation of winner, but does not consider ties.
+            var dictionary = new Dictionary<ICandidate, int>();
+            foreach( var ballot in ballots)
+            {
+                dictionary.TryGetValue(
+                    key: ballot.Vote.Candidate,
+                    value: out var voteCount
+                );
+                dictionary[ballot.Vote.Candidate] = voteCount + 1;
+            }
+            return dictionary.OrderByDescending(x => x.Value).FirstOrDefault().Key;
+        }
+        else
+        {
+            throw new Exception("There are no Ballots and/or Candidates.");
+        }
     }
 }
--- Elections/Elections/RankedChoiceElection.cs
+++ Elections/Elections/RankedChoiceElection.cs
@@ -1,11 +1,43 @@
-﻿using Elections.Interfaces;
+﻿using System.Collections;
+using Elections.Interfaces;
 
 namespace Elections.Elections;
 
 public class RankedChoiceElection : IElection<IRankedBallot>
 {
-    public ICandidate Run(IReadOnlyList<IRankedBallot> ballots, IReadOnlyList<ICandidate> candidates)
+    public ICandidate Run(
+        IReadOnlyList<IRankedBallot> ballots,
+        IReadOnlyList<ICandidate> candidates)
     {
-        throw new NotImplementedException();
+        if ((ballots != null && ballots.Count > 0)
+            && (candidates != null && candidates.Count > 0))
+        {
+            // Note: Initial 'brute force' approximation of winner, but does not consider ties.
+            var dictionary = new Dictionary<ICandidate, int>();
+            foreach( var ballot in ballots)
+            {
+                foreach ( var vote in ballot.Votes )
+                {
+                    dictionary.TryGetValue(
+                        key: vote.Candidate,
+                        value: out var rankNumber
+                    );
+                    if (rankNumber == 0)
+                    {
+                        dictionary[vote.Candidate] = vote.Rank;
+                    }
+                    else
+                    {
+                        dictionary[vote.Candidate] =
+                            dictionary[vote.Candidate] += vote.Rank;
+                    }
+                }
+            }
+            return dictionary.OrderBy(x => x.Value).FirstOrDefault().Key;
+        }
+        else
+        {
+            throw new Exception("There are no Ballots and/or Candidates.");
+        }
     }
 }
