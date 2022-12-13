717949e4cdad6090a814dba0b60a9e9268003d66
Matt Choinski <matt@choinski.net>
2022-12-13 05:59:24 -0500

    Updated with a LINQ implementation of the solution.

--- Elections/Elections/PluralityElection.cs
+++ Elections/Elections/PluralityElection.cs
@@ -12,17 +12,28 @@ public class PluralityElection : IElection<ISingleVoteBallot>
         if ( ( ballots != null && ballots.Count > 0 )
             && ( candidates != null && candidates.Count > 0 ) )
         {
-            // Note: Initial 'brute force' approximation of winner, but does not consider ties.
-            var dictionary = new Dictionary<ICandidate, int>();
-            foreach( var ballot in ballots)
-            {
-                dictionary.TryGetValue(
-                    key: ballot.Vote.Candidate,
-                    value: out var voteCount
-                );
-                dictionary[ballot.Vote.Candidate] = voteCount + 1;
-            }
-            return dictionary.OrderByDescending(x => x.Value).FirstOrDefault().Key;
+            // Note: Initial 'brute force' count of winner, but does not consider ties.
+            //var dictionary = new Dictionary<ICandidate, int>();
+            //foreach (var ballot in ballots)
+            //{
+            //    dictionary.TryGetValue(
+            //        key: ballot.Vote.Candidate,
+            //        value: out var voteCount
+            //    );
+            //    dictionary[ballot.Vote.Candidate] = voteCount + 1;
+            //}
+            //return dictionary.OrderByDescending(x => x.Value).FirstOrDefault().Key;
+
+            // Note: LINQ version, but also does not consider ties.
+            var votesPerCandidate =
+                from ballot in ballots
+                group ballot by ballot.Vote.Candidate into candidateGroup
+                select new
+                {
+                    Candidate = candidateGroup.Key,
+                    Count = candidateGroup.Count()
+                };
+            return votesPerCandidate.OrderByDescending(x => x.Count).FirstOrDefault().Candidate;
         }
         else
         {
