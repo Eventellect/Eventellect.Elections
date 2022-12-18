22aba7e6e7efa49b9006dce2d5c93fa9ff21d7dd
SeanM <SCM987@outlook.com>
2022-12-18 11:04:27 -0800

    Plurality and Ranked Election

--- Elections/Elections/PluralityElection.cs
+++ Elections/Elections/PluralityElection.cs
@@ -4,8 +4,14 @@ namespace Elections.Elections;
 
 public class PluralityElection : IElection<ISingleVoteBallot>
 {
-    public ICandidate Run(IReadOnlyList<ISingleVoteBallot> ballots, IReadOnlyList<ICandidate> candidates)
+    public ICandidate? Run(IReadOnlyList<ISingleVoteBallot> ballots, IReadOnlyList<ICandidate> candidates)
     {
-        throw new NotImplementedException();
+        if (ballots == null || ballots.Count <= 0)
+        {
+            return null;
+        }
+
+        var candidateCount = ballots.GroupBy(ballot => ballot.Vote.Candidate).Select((candidate) => new { Candidate = candidate.Key, Count = candidate.Count() });
+        return candidateCount?.OrderByDescending(candidate => candidate.Count).FirstOrDefault()?.Candidate;
     }
 }
